259018edfbebb3108db8b6d8df517c0b1e6aa07f
Kevin St.Onge <kevinstonge@gmail.com>
2022-01-10 14:40:25 -0500

    more responsive layout

--- components/CurrentTask.tsx
+++ components/CurrentTask.tsx
@@ -6,25 +6,54 @@ import {
   faPause,
   faStopCircle,
 } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 const CurrentTask: NextPage = () => {
+  const [disabledButtons, setDisabledButtons] = useState({
+    play: false,
+    pause: true,
+    stop: true,
+  });
+  //
+  // what will we know from global state?
+  // need to modify my current global state to the following:
+  //   if the user has pressed nothing yet:
+  //     lastInteractionTimestamp: undefined,
+  //     playStatus: "stopped",
+  //     disabledButtons: ["stop", "pause"].
+  //   if the user has pressed play and lastInteractionTimestamp === undefined:
+  //     lastInteractionTimestamp: [[[now]]],
+  //     playStatus: "playing",
+  //     disabledButtons: ["stop", "play"],
+  //   if the user has pressed pause:
+  //     lastInteractionTimestamp: [[[now]]],
+  //     playStatus: "paused",
+  //     disabledButtons: ["pause"]
+  //   if the user has pressed stop:
+  //     lastInteractionTimestamp: [[[now]]],
+  //     playStatus: "stopped",
+  //     disabledButtons: ["stop", "pause"]
+
+  // in this component I need to read global state and toggle the disabled attribute on each button component.
   return (
     <div className={styles.currentTask}>
       <div className={styles.currentTaskLeft}>
+        <p className={styles.bigTime}>Ti:ME:--</p>
         <div className={styles.currentTaskToolbar}>
-          <button data-glow-color="c2">
+          <button data-glow-color="c2" disabled={disabledButtons.play}>
+            {/* disable a subset of these buttons depending on play state: if
+            playing: disable play and stop if paused: disable pause */}
             <FontAwesomeIcon icon={faPlay} />
             <p>play</p>
           </button>
-          <button data-glow-color="c1">
+          <button data-glow-color="c1" disabled={disabledButtons.pause}>
             <FontAwesomeIcon icon={faPause} />
             <p>pause</p>
           </button>
-          <button data-glow-color="e1">
+          <button data-glow-color="e1" disabled={disabledButtons.stop}>
             <FontAwesomeIcon icon={faStopCircle} />
             <p>stop</p>
           </button>
         </div>
-        <p>Ti:ME:--</p>
       </div>
       <div className={styles.currentTaskRight}>
         <h3>task name</h3>
