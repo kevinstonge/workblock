2e52e5a663ca57daf98d31ebd88abb9172bc9b04
Kevin St.Onge <kevinstonge@gmail.com>
2022-01-15 16:25:03 -0500

    ...

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -4,126 +4,39 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faPlay } from '@fortawesome/free-solid-svg-icons';
 import { useContext } from 'react';
 import { store } from '../state/store';
+import { TaskFull, TaskShort } from '../utils/types';
 const TaskList: NextPage = () => {
   const {
-    state: { activeBlockID, tasks, timestamp, duration },
+    state: { activeBlockID, tasks, blocks },
     dispatch,
   } = useContext(store);
-  const fullTaskList: TaskFull[] = tasks.filter((task) => {});
+  const fullTaskList = blocks[activeBlockID].taskSchedule.map((b) => {
+    return tasks.filter((t) => t.id === b.taskID);
+  });
+  console.log(fullTaskList);
   return (
     <div className={styles.taskList}>
       <h2>Task List:</h2>
-      <ul></ul>
       <ul>
-        {/* <li className={styles.taskListItem + ' ' + styles.taskListItemActive}> */}
-        <li className={styles.taskListItem + ' ' + styles.taskListItemActive}>
-          <div>
-            <FontAwesomeIcon icon={faPlay} />
-            <p className={styles.taskTitle}>task_title asdfasdf asdfa sdf</p>
-          </div>
-          <div>
-            <p>aasdf hasdfkl asdhflaks dhfalksdhf alksdhf alskdhf alskdhf asdhflkasdh flaksdhf</p>
-            <button>
-              <FontAwesomeIcon icon={faEye} />
-            </button>
-          </div>
-        </li>
-        <li className={styles.taskListItem}>
-          <div>
-            <FontAwesomeIcon icon={faPlay} />
-            <p className={styles.taskTitle}>task_title asdfasdf asdfa sdf</p>
-          </div>
-          <div>
-            <p>aasdf hasdfkl asdhflaks dhfalksdhf alksdhf alskdhf alskdhf asdhflkasdh flaksdhf</p>
-            <button>
-              <FontAwesomeIcon icon={faEye} />
-            </button>
-          </div>
-        </li>
-        <li className={styles.taskListItem}>
-          <div>
-            <FontAwesomeIcon icon={faPlay} />
-            <p className={styles.taskTitle}>task_title asdfasdf asdfa sdf</p>
-          </div>
-          <div>
-            <p>aasdf hasdfkl asdhflaks dhfalksdhf alksdhf alskdhf alskdhf asdhflkasdh flaksdhf</p>
-            <button>
-              <FontAwesomeIcon icon={faEye} />
-            </button>
-          </div>
-        </li>
-        <li className={styles.taskListItem}>
-          <div>
-            <FontAwesomeIcon icon={faPlay} />
-            <p className={styles.taskTitle}>task_title asdfasdf asdfa sdf</p>
-          </div>
-          <div>
-            <p>aasdf hasdfkl asdhflaks dhfalksdhf alksdhf alskdhf alskdhf asdhflkasdh flaksdhf</p>
-            <button>
-              <FontAwesomeIcon icon={faEye} />
-            </button>
-          </div>
-        </li>
-        <li className={styles.taskListItem}>
-          <div>
-            <FontAwesomeIcon icon={faPlay} />
-            <p className={styles.taskTitle}>task_title asdfasdf asdfa sdf</p>
-          </div>
-          <div>
-            <p>aasdf hasdfkl asdhflaks dhfalksdhf alksdhf alskdhf alskdhf asdhflkasdh flaksdhf</p>
-            <button>
-              <FontAwesomeIcon icon={faEye} />
-            </button>
-          </div>
-        </li>
-        <li className={styles.taskListItem}>
-          <div>
-            <FontAwesomeIcon icon={faPlay} />
-            <p className={styles.taskTitle}>task_title asdfasdf asdfa sdf</p>
-          </div>
-          <div>
-            <p>aasdf hasdfkl asdhflaks dhfalksdhf alksdhf alskdhf alskdhf asdhflkasdh flaksdhf</p>
-            <button>
-              <FontAwesomeIcon icon={faEye} />
-            </button>
-          </div>
-        </li>
-        <li className={styles.taskListItem}>
-          <div>
-            <FontAwesomeIcon icon={faPlay} />
-            <p className={styles.taskTitle}>task_title asdfasdf asdfa sdf</p>
-          </div>
-          <div>
-            <p>aasdf hasdfkl asdhflaks dhfalksdhf alksdhf alskdhf alskdhf asdhflkasdh flaksdhf</p>
-            <button>
-              <FontAwesomeIcon icon={faEye} />
-            </button>
-          </div>
-        </li>
-        <li className={styles.taskListItem}>
-          <div>
-            <FontAwesomeIcon icon={faPlay} />
-            <p className={styles.taskTitle}>task_title asdfasdf asdfa sdf</p>
-          </div>
-          <div>
-            <p>aasdf hasdfkl asdhflaks dhfalksdhf alksdhf alskdhf alskdhf asdhflkasdh flaksdhf</p>
-            <button>
-              <FontAwesomeIcon icon={faEye} />
-            </button>
-          </div>
-        </li>
-        <li className={styles.taskListItem}>
-          <div>
-            <FontAwesomeIcon icon={faPlay} />
-            <p className={styles.taskTitle}>task_title asdfasdf asdfa sdf</p>
-          </div>
-          <div>
-            <p>aasdf hasdfkl asdhflaks dhfalksdhf alksdhf alskdhf alskdhf asdhflkasdh flaksdhf</p>
-            <button>
-              <FontAwesomeIcon icon={faEye} />
-            </button>
-          </div>
-        </li>
+        {fullTaskList.map((task: TaskFull[]) => {
+          const isActive = true; //just need to set this
+          const className =
+            `${styles.taskListItem}` + (isActive ? ` ${styles.taskListItemActive}` : '');
+          return (
+            <li className={className}>
+              <div>
+                <FontAwesomeIcon icon={faPlay} />
+                <p className={styles.taskTitle}>{task[0].taskTitle}</p>
+              </div>
+              <div>
+                <p>{task[0].taskDescription}</p>
+                <button>
+                  <FontAwesomeIcon icon={faEye} />
+                </button>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
